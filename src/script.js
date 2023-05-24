4ef08b5ff7d72304cfe417bb6819f6b386b27ecb
ElWaje <elwaje@gmail.com>
2023-05-24 20:52:01 +0200

    Añadir boton cambio color

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ boton_1.addEventListener('click', () => {
 
 const boton_2 = document.getElementById('cambiarColor_2')
 
+
 boton_2.addEventListener('click', () => {
   const body = document.querySelector('body')
   body.style.backgroundColor = 'blue'
